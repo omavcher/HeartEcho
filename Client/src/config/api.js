44011f7e66d09b0c07687578e305cd64cc1a073b
Om Avcher <153804283+omavcher@users.noreply.github.com>
2025-08-27 13:01:25 +0530

    Update api.js

--- Client/src/config/api.js
+++ Client/src/config/api.js
@@ -1,6 +1,6 @@
 const api = {
-    Url: 'https://heartecho.onrender.com/api/v1/api',
-    // Url: 'http://localhost:5000/api/v1/api'
+    // Url: 'https://heartecho.onrender.com/api/v1/api',
+    Url: 'https://heartecho-d851.onrender.com/api/v1/api'
 
 };
 export default api;
