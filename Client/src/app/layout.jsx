4819a7cf279b5358f870944bc31423aa2f6cd112
Om Avcher <omawchar07@gmial.com>
2025-12-17 12:11:08 +0530

    Daily Check

--- Client/src/app/layout.jsx
+++ Client/src/app/layout.jsx
@@ -138,14 +138,17 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8343501385468147"
           crossOrigin="anonymous"
         />
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4W14R3SYMY"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-4W14R3SYMY');
-</script>
+ <script async src="https://www.googletagmanager.com/gtag/js?id=G-4W14R3SYMY"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4W14R3SYMY');
+            `,
+          }}
+        />
       </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
