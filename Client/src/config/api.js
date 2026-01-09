6829f352aa591d70e2984dd0413dc460cbdcedcc
Om Avcher <omawchar07@gmial.com>
2026-01-09 11:40:38 +0530

    Daily Update

--- Client/src/config/api.js
+++ Client/src/config/api.js
@@ -1,11 +1,11 @@
 const api = {
     // Url: 'https://heartecho.onrender.com/api/v1/api',/
     // heartecho0007@gmail.com
-    // Url: 'https://heartecho-lm9j.onrender.com/api/v1/api',$$
+    Url: 'https://heartecho-lm9j.onrender.com/api/v1/api',
 
     // omawchar07@gmail.com
     // Url: 'https://heartecho-d851.onrender.com/api/v1/api',
-    Url: 'http://localhost:5000/api/v1/api'
+    // Url: 'http://localhost:5000/api/v1/api'
 
 };
 export default api;
