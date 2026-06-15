4332298738da262317eb76e11b95eaa4b85fbe20
Om Avcher <omawchar07@gmial.com>
2026-06-15 21:22:48 +0530

    pixel added

--- Client/src/components/Subscriptions.jsx
+++ Client/src/components/Subscriptions.jsx
@@ -273,27 +273,8 @@ function SubscriptionContent() {
 
   return (
     <div className="seh-page-wrapper">
+      {/* Razorpay SDK */}
       <Script src="https://checkout.razorpay.com/v1/checkout.js" strategy="lazyOnload" />
-
-      {/* ── Meta Pixel — afterInteractive (fires on subscription page) ── */}
-      <Script
-        id="fb-pixel-new"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '936362649426203');
-            fbq('track', 'PageView');
-          `,
-        }}
-      />
       <noscript>
         <img height="1" width="1" style={{ display: 'none' }}
           src="https://www.facebook.com/tr?id=936362649426203&ev=PageView&noscript=1"
